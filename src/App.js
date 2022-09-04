e2665685206f35d643e81a884329cdd919287966
Raj Vijay Jadhav <76254854+FrozenSamurai@users.noreply.github.com>
2022-09-04 10:11:51 +0530

    team view works well few changes required

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
+import New from "./New";
+
+function App() {
+  const [mouseCoords, setMouseCoords] = useState(null);
+  // const obj = {
+  //   Yash: {
+  //     height: "0px",
+  //     front: "/Yash/front.jpeg",
+  //     top: "/Yash/top.jpeg",
+  //     buttom: "/Yash/buttom.jpeg",
+  //     left: "/Yash/left.jpeg",
+  //     right: "/Yash/right.jpeg",
+  //     topright: "/Yash/topright.jpeg",
+  //     topleft: "/Yash/topleft.jpeg",
+  //     buttomright: "/Yash/buttomright.jpeg",
+  //     buttomleft: "/Yash/buttomleft.jpeg",
+  //   },
+  //   Raj: {
+  //     height: "-180px",
+  //     front: "/Raj/front.jpeg",
+  //     top: "/Raj/top.jpeg",
+  //     buttom: "/Raj/buttom.jpeg",
+  //     left: "/Raj/left.jpeg",
+  //     right: "/Raj/right.jpeg",
+  //     topright: "/Raj/topright.jpeg",
+  //     topleft: "/Raj/topleft.jpeg",
+  //     buttomright: "/Raj/buttomright.jpeg",
+  //     buttomleft: "/Raj/buttomleft.jpeg",
+  //   },
+  // };
+
+  const obj = {
+    Yash: "/allmembers/Yash.jpeg",
+    Raj: "/allmembers/Raj.jpeg",
+    Dhruv: "/allmembers/Dhruv.jpeg",
+    Mrunmayee: "/allmembers/Mrunmayee.jpeg",
+    Aarushi: "/allmembers/Aarushi.jpeg",
+    Deep: "/allmembers/Deep.jpeg",
+    Manas: "/allmembers/Manas.jpeg",
+    Nirmiti: "/allmembers/Nirmiti.jpeg",
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    const body = document.getElementsByTagName("body")[0];
+    body.style.height = "100%";
+    body.onmousemove = (e) => {
+      setMouseCoords({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+  }, []);
+
+  return (
+    <div
+      className="flex justify-center items-center space-x-4"
+      style={{ minHeight: "100vh" }}
+    >
+      {mouseCoords &&
+        Object.keys(obj).map((key) => {
+          return (
+            <New key={key} name={obj[key]} mouseCoords={mouseCoords} Id={key} />
+          );
+        })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/New.js
@@ -0,0 +1,113 @@
+import React, { useRef, useEffect, useState } from "react";
+
+const New = ({ name, mouseCoords, Id }) => {
+  const eid = useRef();
+  const ref = useRef();
+  const [activePhoto, setActivePhoto] = useState(null);
+  const [onDiv, setOnDiv] = useState(false);
+
+  useEffect(() => {
+    let random = eid.current.getBoundingClientRect();
+    let left = random.left;
+    let width = random.width;
+    // let top = eid.current.offsetTop;
+    let top = random.top;
+    let height = random.height;
+
+    let centerX = left + width / 2;
+    let centerY = top + height / 2;
+    // console.log(centerX, centerY);
+
+    let mouseX = mouseCoords.x - centerX;
+    let mouseY = -(mouseCoords.y - centerY);
+
+    // console.log(ev)
+
+    let rad = Math.atan2(mouseY, mouseX); // In radians
+    let deg = rad * (180 / Math.PI);
+    // !onDiv
+    //   ? (ref.current.style.backgroundImage = `url(${name})`)
+    //   : (ref.current.style.backgroundImage = `url(/Raj/front.jpeg)`);
+    // console.log(name.front, deg);
+    //update css according to current angle
+    // if (deg < 22.5 && deg >= -22.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.right})`;
+    //   setActivePhoto("right");
+    // } else if (deg < 67.5 && deg >= 22.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.topright})`;
+    //   setActivePhoto("topright");
+    // } else if (deg < 112.5 && deg >= 67.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.top})`;
+    //   setActivePhoto("top");
+    // } else if (deg < 157.5 && deg >= 112.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.topleft})`;
+    //   setActivePhoto("topleft");
+    // } else if ((deg < 180 && deg >= 157.5) || (deg < -157.5 && deg >= -180)) {
+    //   // eid.current.style.backgroundImage = `url(${name.left})`;
+    //   setActivePhoto("left");
+    // } else if (deg < -112.5 && deg >= -157.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.buttomleft})`;
+    //   setActivePhoto("buttomleft");
+    // } else if (deg < -67.5 && deg >= -112.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.buttom})`;
+    //   setActivePhoto("buttom");
+    // } else if (deg < -22.5 && deg >= -67.5) {
+    //   // eid.current.style.backgroundImage = `url(${name.buttomright})`;
+    //   setActivePhoto("buttomright");
+    // }
+    let w;
+    if (onDiv) {
+      w = "15px";
+      ref.current.style.backgroundSize = "cover";
+    } else {
+      ref.current.style.backgroundSize = "";
+      if (deg < 22.5 && deg >= -22.5) {
+        w = "-150px";
+      } else if (deg < 67.5 && deg >= 22.5) {
+        w = "-290px";
+      } else if (deg < 112.5 && deg >= 67.5) {
+        w = "-430px";
+      } else if (deg < 157.5 && deg >= 112.5) {
+        w = "-590px";
+      } else if ((deg < 180 && deg >= 157.5) || (deg < -157.5 && deg >= -180)) {
+        w = "-730px";
+      } else if (deg < -112.5 && deg >= -157.5) {
+        w = "-860px";
+      } else if (deg < -67.5 && deg >= -112.5) {
+        w = "-1000px";
+      } else if (deg < -22.5 && deg >= -67.5) {
+        w = "-1150px";
+      }
+    }
+    let x = w + " " + "0px";
+    ref.current.style.backgroundPosition = x;
+  }, [mouseCoords]);
+
+  return (
+    <div id="eid" ref={eid} className="">
+      {/* {Object.keys(name).map((key, index) => (
+        <img
+          id={name}
+          key={index}
+          src={name[key]}
+          alt={key}
+          className=" w-20 "
+          style={{ display: activePhoto === key ? "block" : "none" }}
+          onMouseOver={() => setActivePhoto("front")}
+        />
+        
+      ))} */}
+      <div
+        ref={ref}
+        id={Id}
+        className="w-32 h-32 rounded-full"
+        style={{ backgroundImage: `url(${name})` }}
+        onMouseOver={() => setOnDiv(true)}
+        onMouseLeave={() => setOnDiv(false)}
+        title={Id}
+      ></div>
+    </div>
+  );
+};
+
+export default New;
